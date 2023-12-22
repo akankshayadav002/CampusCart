6d6f4a16e19cf7e6028e14d94b451b3f2ae989f3
AKANKSHAY9 <84265568+AKANKSHAY9@users.noreply.github.com>
2023-12-22 15:19:04 +0530

    sign up

--- server/index.js
+++ server/index.js
@@ -1,6 +1,35 @@
 import express from 'express'
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+
+const uri = "mongodb+srv://akanksha:Lappy@89@cluster0.sbkle3g.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 const app=express();
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 })
+
